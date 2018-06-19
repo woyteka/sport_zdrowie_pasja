2d97b2f0fd36377a8e91fad58e11b469f988ed95
woyteka <40150050+woyteka@users.noreply.github.com>
2018-06-19 22:13:23 +0200

    Skonczona pierwsza strona

---
+++ js/animation.js
@@ -0,0 +1,9 @@
+$(function(){
+    $(".dbox_zpasji").animate({width:"0px" });
+    
+    $(".dbox_zpasji").animate({width:"100px"});
+    
+    
+    
+    $(".dtxt_zpasji").animate({left:"-1px"},1000);
+})
