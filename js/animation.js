c2aaf952acf5757c15fedcfd678d980818c88ce8
woyteka <40150050+woyteka@users.noreply.github.com>
2018-06-25 22:27:44 +0200

    Przygotowanie podstrony sport

--- js/animation.js
+++ js/animation.js
@@ -26,6 +26,27 @@ $(function(){
     
               $(".box_button").animate({opacity:0 },1000); 
      $(".box_button").animate({opacity:1 },1000);
+    
+    
+    
+    
+    
+     $(".box_witam").animate({opacity:0 }); 
+   $(".box_witam").animate({opacity:1 },100);
+    
+         $(".txt_nazywamsie").animate({opacity:0 }); 
+   $(".txt_nazywamsie").animate({opacity:1 },300);
+    
+    
+           $(".txt_rafal").animate({opacity:0 }); 
+   $(".txt_rafal").animate({opacity:1 },600);
+    
+       $(".box_kensik").animate({opacity:0 }); 
+   $(".box_kensik").animate({opacity:1 },1200);
+    
+           $(".txt_tresc_rafal").animate({opacity:0 }); 
+   $(".txt_tresc_rafal").animate({opacity:1 },1200);
+    
 });
 
 
